2977fde4a053956332c131711730eea37185aa54
FiveChjr <f5.chjr@gmail.com>
2019-02-13 16:49:51 +0700

    (fix)

--- dist/routes/route.commons.js
+++ dist/routes/route.commons.js
@@ -149,11 +149,12 @@ class Routes {
             //     }],
             //     group: ['User.bloodType']
             // })
-            util_database_1.sequelize.query('SELECT users."bloodType", count(sessions.id) as n FROM users LEFT JOIN sessions on users.id = sessions."userId" GROUP BY users."bloodType"', { type: util_database_1.sequelize.QueryTypes.SELECT }).then(d => {
+            util_database_1.sequelize.query('SELECT users."bloodType", count(sessions.id) as count FROM users LEFT JOIN sessions on users.id = sessions."userId" GROUP BY users."bloodType" WHERE year(sessions."checkOut") = ?', { replacements: [req.params.year], type: util_database_1.sequelize.QueryTypes.SELECT }).then(d => {
                 console.log(chalk_1.default.bgYellow(d));
                 util_response_1.apiResponse(res, 200, d, null, false, req.cacheKey, 60);
             }).catch(e => {
                 console.log(e);
+                util_response_1.apiResponse(res, 400);
             });
         }));
         // this.router.get('/insights/blood-types', [
