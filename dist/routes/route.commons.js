b80ac117fef1a8b1f973ed13e599c5c437b9840d
FiveChjr <f5.chjr@gmail.com>
2019-02-13 16:43:35 +0700

    (fix)

--- dist/routes/route.commons.js
+++ dist/routes/route.commons.js
@@ -149,7 +149,7 @@ class Routes {
             //     }],
             //     group: ['User.bloodType']
             // })
-            util_database_1.sequelize.query('SELECT count(sessions.id) as Count FROM users LEFT JOIN sessions on users.id = sessions."userId" GROUP BY users."bloodType"', { type: util_database_1.sequelize.QueryTypes.SELECT }).then(d => {
+            util_database_1.sequelize.query('SELECT bloodType, count(sessions.id) as Count FROM users LEFT JOIN sessions on users.id = sessions."userId" GROUP BY users."bloodType"', { type: util_database_1.sequelize.QueryTypes.SELECT }).then(d => {
                 console.log(chalk_1.default.bgYellow(d));
                 util_response_1.apiResponse(res, 200, d, null, false, req.cacheKey, 60);
             }).catch(e => {
