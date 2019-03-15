b2e2185bc12e01aa8a7b620dbefa9f27dcc4110e
FiveChjr <f5.chjr@gmail.com>
2019-03-15 10:51:29 +0700

    (fix): add day of the week

--- src/routes/route.commons.ts
+++ src/routes/route.commons.ts
@@ -54,7 +54,7 @@ class Routes {
                     order: [['id', 'ASC']],
                 }
                 let timeOptions = {
-                    attributes: ['id', 'startTime', 'endTime', 'label', 'isSunday', 'isMonday', 'isTuesday', 'isWednesday', 'isThursday', 'isFriday', 'isSunday'],
+                    attributes: ['id', 'startTime', 'endTime', 'label', 'isSunday', 'isMonday', 'isTuesday', 'isWednesday', 'isThursday', 'isFriday', 'isSaturday'],
                     order: [['id', 'ASC']],
                 }
                 let data = await Project.findAll(projectOptions)
